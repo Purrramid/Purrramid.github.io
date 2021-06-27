71bcb1e0d01f525b5008253275aed74a187b340c
vortami <27891438+vortami@users.noreply.github.com>
2021-06-27 03:34:12 +0200

    rotate games, add new layout for mobile

--- src/components/Games.js
+++ src/components/Games.js
@@ -27,11 +27,12 @@ class Games extends React.Component {
 			<div className="my-3">
 				<h2 id="games">Games</h2>
 				<p>We play a lot of games, and when we say that, we mean it. Here's just a small list of the games we play.</p>
-				<games className="d-grid grid-4">
+				<games className="d-grid grid-5">
 					<Game name="Tom Clancy's Rainbow Six Siege" href="https://rainbow6.com" />
-					<Game name="Apex Legends" href="https://apexlegends.com" />
-					<Game name="Valheim" href="https://valheimgame.com" />
 					<Game name="VALORANT" href="https://playvalorant.com" />
+					<Game name="Dead by Daylight" href="https://deadbydaylight.com" />
+					<Game name="Battlefield 4" href="https://www.ea.com/games/battlefield/battlefield-4" />
+					<Game name="Apex Legends" href="https://apexlegends.com" />
 				</games>
 			</div>
 		);
